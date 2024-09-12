9663fb6332e5ad7ab1db2e4a673460dc2e47b87d
day0xy <jasonfan1337@gmail.com>
2024-09-12 10:45:16 +0800

    format

--- UniswapV2/v2-periphery/interfaces/IUniswapV2Migrator.sol
+++ UniswapV2/v2-periphery/interfaces/IUniswapV2Migrator.sol
@@ -1,5 +1,6 @@
 pragma solidity >=0.5.0;
 
+// 该函数将流动性从 Uniswap V1 迁移到 Uniswap V2
 interface IUniswapV2Migrator {
     function migrate(address token, uint amountTokenMin, uint amountETHMin, address to, uint deadline) external;
 }
